2b3822342fc39ff620c0dac5b8b5d2feff94a92b
Lord-sarcastic <adeoti.15.jude@gmail.com>
2020-06-08 23:12:07 +0100

    Chore: Added Static_root for static files.

--- Authenticated/Authenticated/Authenticated/settings.py
+++ Authenticated/Authenticated/Authenticated/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
