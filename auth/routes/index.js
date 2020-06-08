031229f98f0443ec8b8ece60d0b41a1c2d807a00
najibkado <30640486+najibkado@users.noreply.github.com>
2020-06-08 22:13:23 +0400

    Added login, register and logout end points
    Authentication was done using passportjs thus, hashing and salting was added on passwords. MongoDB was used as database where userDB is the database name.

--- auth/routes/index.js
+++ auth/routes/index.js
@@ -1,6 +1,79 @@
+//jshint esversion:6
 import { Router } from "express";
+import app from "./app";
+require("dotenv").config();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const router = Router();
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+router.post("/login", function(req, res) {
+
+  const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+  });
+
+  req.login(user, function (err) {
+      if (err) {
+          console.log(err);
+          res.send("error");
+      } else {
+          passport.authenticate("local")(req, res, function () {
+              res.send("success");
+          })
+      }
+  });
+
+});
+
+
+router.post("/register", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  User.register({username: username}, password, function (err, user) {
+      if (!err) {    
+          passport.authenticate("local")(req, res, function () {
+              res.send("success");
+          });
+      } else {
+          console.log(err);
+          res.send("error");
+      }
+  });
+});
+
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.send("success");
+});
+
+
+
+
 
 router.get("/hello", function (req, res) {
     return res.status(200).send({
@@ -8,5 +81,7 @@ router.get("/hello", function (req, res) {
           message: "Hello world"
         });
 } );
+
+
 
 export default router;
