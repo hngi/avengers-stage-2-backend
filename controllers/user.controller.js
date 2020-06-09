e04c8288bac92636bca226d712c6d259009b4f9f
Fatai Balogun <fattylee.remod@gmail.com>
2020-06-09 16:13:15 +0100

    add pseudocode for forgotpassword feature

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,170 +1,178 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-const User = require('../models/user.model')
-const MailerUtil = require('../mail/index')
-const TokenUtil = require('../util/token')
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user.model");
+const MailerUtil = require("../mail/index");
+const TokenUtil = require("../util/token");
+require("dotenv").config();
 
 //Load Input Validation
-const validateRegisterInput = require('../validation/register')
-const validateLoginInput = require('../validation/login')
-const validateResetInput = require('../validation/reset')
-const validateForgetInput = require('../validation/forget')
-const validateChangeInput = require('../validation/change')
+const validateRegisterInput = require("../validation/register");
+const validateLoginInput = require("../validation/login");
+const validateResetInput = require("../validation/reset");
+const validateForgetInput = require("../validation/forget");
+const validateChangeInput = require("../validation/change");
 
 exports.registerUser = (req, res, next) => {
-  const { errors, isValid } = validateRegisterInput(req.body)
+  const { errors, isValid } = validateRegisterInput(req.body);
 
   //Check Validation
   if (!isValid) {
-    return res.status(400).send({ response: errors })
+    return res.status(400).send({ response: errors });
   }
 
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
-  User.findOne({ email: req.body.email }).then(user => {
+  User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
-      errors.email = 'Email already exists'
-      return res.status(400).send({ response:  errors })
+      errors.email = "Email already exists";
+      return res.status(400).send({ response: errors });
     } else {
       const newUser = new User({
         email,
-        password
-      })
+        password,
+      });
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err
-          newUser.password = hash
+          if (err) throw err;
+          newUser.password = hash;
           newUser
             .save()
-            .then(user =>{
-                // const msg = `<b>Hello ${user.email}<b><br /><p> Thank you for registering with us</p><br /> <p><i> Team Avengers </i></p>`
-                // MailerUtil.sendMail(user.email, 'Registration Successful', msg ).then((e) => {
-                //   res.status(200).send({ success: true, data: user })
-                // }).catch(() => {
-                  res.status(200).send({ success: true, data: user })
-                // })
-              }
-            )
-            .catch(err => console.log(err))
-        })
-      })
+            .then((user) => {
+              // const msg = `<b>Hello ${user.email}<b><br /><p> Thank you for registering with us</p><br /> <p><i> Team Avengers </i></p>`
+              // MailerUtil.sendMail(user.email, 'Registration Successful', msg ).then((e) => {
+              //   res.status(200).send({ success: true, data: user })
+              // }).catch(() => {
+              res.status(200).send({ success: true, data: user });
+              // })
+            })
+            .catch((err) => console.log(err));
+        });
+      });
     }
-  })
-}
+  });
+};
 
 exports.loginUser = (req, res, next) => {
-  const { errors, isValid } = validateLoginInput(req.body)
+  const { errors, isValid } = validateLoginInput(req.body);
 
   //Check if the login is valid
   if (!isValid) {
-    return res.status(400).send({ response: errors })
+    return res.status(400).send({ response: errors });
   }
 
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
-  User.findOne({ email }).then(user => {
-
+  User.findOne({ email }).then((user) => {
     if (!user) {
-      return res.status(423).send({ response: 'User does not exist, kindly register!!'})
+      return res
+        .status(423)
+        .send({ response: "User does not exist, kindly register!!" });
     }
 
     bcrypt
       .compare(password, user.password)
-      .then(valid => {
+      .then((valid) => {
         if (!valid) {
-          return res.status(403).send({ response: 'Incorrect username or password' })
+          return res
+            .status(403)
+            .send({ response: "Incorrect username or password" });
         }
 
         const token = TokenUtil.signedJWT(email);
 
         return res.status(200).send({
           success: true,
-          token
-        })
+          token,
+        });
       })
-      .catch(err => console.log(err))
-  })
-}
+      .catch((err) => console.log(err));
+  });
+};
 
 exports.checkToken = (req, res, next) => {
-  const header = req.headers['authorization'];
+  const header = req.headers["authorization"];
 
-  if(typeof header !== 'undefined') {
-    const token = header.split(' ')[1];
+  if (typeof header !== "undefined") {
+    const token = header.split(" ")[1];
 
     req.token = token;
     next();
   } else {
     res.sendStatus(403);
   }
-}
+};
 
 exports.resetPassword = (req, res) => {
-  const { errors, isValid } = validateResetInput(req.body)
+  const { errors, isValid } = validateResetInput(req.body);
 
   //Check Validation
   if (!isValid) {
-    return res.status(400).send({ response: errors })
+    return res.status(400).send({ response: errors });
   }
 
-  const {old_password, new_password} = req.body;
+  const { old_password, new_password } = req.body;
   jwt.verify(req.token, process.env.SECRET_TOKEN, async (err, userData) => {
-    if(err) {
-      res.status(401).json({response: 'Unauthorised access'});
+    if (err) {
+      res.status(401).json({ response: "Unauthorised access" });
     } else {
       const { email } = userData;
       try {
         let user = await User.findOne({ email });
         if (!user.password) {
-          let isPasswordMatched = await bcrypt.compare(old_password, user.password)
+          let isPasswordMatched = await bcrypt.compare(
+            old_password,
+            user.password
+          );
           if (isPasswordMatched) {
-            let hashedPassword = await bcrypt.hash(new_password, 10)
+            let hashedPassword = await bcrypt.hash(new_password, 10);
             user.password = hashedPassword;
-            user = await user.save()
+            user = await user.save();
             // const msg = `<b>Hello ${email}<b><br /><p> Your password was succefully changed, if you did't initialize this, please contact us</p><br /> <p><i> Team Avengers </i></p>`
             // MailerUtil.sendMail(email, 'Password Changed', msg).then((e) => {
             //   return res.status(200).send({ success: true, response: 'Password successfully reset' })
             // }).catch(() => {
-              return res.status(200).send({ success: true, response: 'Password successfully reset' })
+            return res
+              .status(200)
+              .send({ success: true, response: "Password successfully reset" });
             // })
           } else {
-            res.status(403).send({ response: 'Incorrect old password' });
+            res.status(403).send({ response: "Incorrect old password" });
           }
         } else {
-          let hashedPassword = await bcrypt.hash(new_password, 10)
+          let hashedPassword = await bcrypt.hash(new_password, 10);
           user.password = hashedPassword;
           user = await user.save();
           // const msg = `<b>Hello ${email}<b><br /><p> Your password was succefully changed, if you did't initialize this, please contact us</p><br /> <p><i> Team Avengers </i></p>`
           // MailerUtil.sendMail(email, 'Password Changed', msg).then((e) => {
           //   return res.status(200).send({ success: true, response: 'Password successfully reset' })
           // }).catch(() => {
-            return res.status(200).send({ success: true, response: 'Password successfully reset' })
+          return res
+            .status(200)
+            .send({ success: true, response: "Password successfully reset" });
           // })
         }
-      } catch (e) {
-        
-      }
+      } catch (e) {}
     }
-  })
-}
+  });
+};
 
 exports.reset = async (req, res) => {
-  const { errors, isValid } = validateForgetInput(req.body)
+  const { errors, isValid } = validateForgetInput(req.body);
 
   //Check Validation
   if (!isValid) {
-    return res.status(400).send({ response: errors })
+    return res.status(400).send({ response: errors });
   }
 
-  const {email} = req.body;
+  const { email } = req.body;
   const token = await TokenUtil.generateEmailToken(email);
 
   User.findOne({ email }, function(err, user) {
     if (!user) {
-      return res.status(400).send({ response: 'No account with that email address exists.' })
-    }
-    else {
+      return res
+        .status(400)
+        .send({ response: "No account with that email address exists." });
+    } else {
       user.resetPasswordToken = token;
       user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
@@ -177,53 +185,100 @@ exports.reset = async (req, res) => {
         // MailerUtil.sendMail(user.email, subject, msg ).then((e) => {
         //   res.status(200).send({ success: true, url: urll, msg })
         // }).catch(() => {
-          res.status(200).send({ success: true, url, msg })
+        res.status(200).send({ success: true, url, msg });
         // })
-      })
+      });
     }
   });
-}
+};
 
 exports.changePassword = function(req, res) {
-  const { errors, isValid } = validateChangeInput(req.body)
+  const { errors, isValid } = validateChangeInput(req.body);
 
   //Check Validation
   if (!isValid) {
-    return res.status(400).send({ response: errors })
+    return res.status(400).send({ response: errors });
   }
 
-  const {password} = req.body;
-  const {token} = req.params;
+  const { password } = req.body;
+  const { token } = req.params;
 
-  User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } }, (err, user) => {
-    if(err){
-      return res.status(400).send({ response: 'Error changing password' })
+  User.findOne(
+    { resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } },
+    (err, user) => {
+      if (err) {
+        return res.status(400).send({ response: "Error changing password" });
+      }
+
+      if (!user) {
+        return res.status(400).send({
+          response: "Password reset token is invalid or has expired.",
+        });
+      }
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+          if (err) {
+            return res
+              .status(400)
+              .send({ response: "Error changing password" });
+          }
+
+          user.resetPasswordToken = null;
+          user.resetPasswordExpires = null;
+          user.password = hash;
+
+          user.save((err, data) => {
+            const subject = "Password Changed";
+            const msg = `Hello,\n\nThis is a confirmation that the password for your account ${user.email} has just been changed.\n`;
+            MailerUtil.sendMail(user.email, subject, msg)
+              .then((e) => {
+                res
+                  .status(200)
+                  .send({ success: true, response: "Password changed" });
+              })
+              .catch(() => {
+                res
+                  .status(200)
+                  .send({ success: true, response: "Password changed" });
+              });
+          });
+        });
+      });
     }
+  );
+};
 
-    if (!user) {
-      return res.status(400).send({ response: 'Password reset token is invalid or has expired.' })
-    }
+exports.forgotPassword = (req, res, next) => {
+  const { errors, isValid } = validateLoginInput(req.body);
 
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) {
-          return res.status(400).send({ response: 'Error changing password' })
+  //Check if the login is valid
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.status(423).send("User does not exist, kindly register!!");
+      }
+      const token = jwt.sign(
+        {
+          email,
+        },
+        "secrettoken",
+        {
+          expiresIn: "1hr",
         }
+      );
+      // send email to the user with the token embed in it
+      // const hrefLink = `/reset-password/${token}`;
+      // sendMail(hrefLink);
 
-        user.resetPasswordToken = null;
-        user.resetPasswordExpires = null;
-        user.password = hash
-
-        user.save((err, data) => {
-          const subject = 'Password Changed';
-          const msg = `Hello,\n\nThis is a confirmation that the password for your account ${user.email} has just been changed.\n`;
-          MailerUtil.sendMail(user.email, subject, msg ).then((e) => {
-            res.status(200).send({ success: true, response: 'Password changed' })
-          }).catch(() => {
-            res.status(200).send({ success: true, response: 'Password changed' })
-          })
-        })
-      })
+      return res.status(200).send({
+        message: "kindly check your email for a password reset link",
+      });
     })
-  }) 
-}
+    .catch((err) => console.log(err));
+};
