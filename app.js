e26f8277259b59f638d8241b10e6ae1465350376
tochukwu <teeceemum@gmail.com>
2020-06-10 17:41:38 +0100

    simple and full functioning passport google oauth2

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express()
 const mongoose = require('mongoose')
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
+const passport = require('passport')
 require('dotenv').config()
 
 //Call in the routes
 const users = require('./routers/users.route')
+const authRoute = require('./routers/auth')
 
 //DB Config
-let db = process.env.MONGODB_URI
+//let db = process.env.MONGODB_URI
 
 //switch between docker image and atlas
  if (process.env.DOCKER_DB) {
@@ -27,9 +29,11 @@ mongoose
 // Initializing express json Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(passport.initialize());
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1', users);
+app.use('/',authRoute)
 
 //open the port
 app.listen(process.env.PORT)
