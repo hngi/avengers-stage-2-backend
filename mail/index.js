77b53059e9ef7079fdb1fe2e9c001e43cf5d87d6
aveirahabeeb <aveirahabeeb@gmail.om>
2020-06-10 16:12:10 +0100

    Added Sign in mail feature @aveira

--- mail/index.js
+++ mail/index.js
@@ -4,13 +4,14 @@
 
 
 const sendGrid = require('@sendgrid/mail');
+const dateFormat = require('dateformat');
 
 sendGrid.setApiKey(process.env.SENDGRID_APIKEY);
 
 module.exports = {
 
   /* code for Sign Up mail here */
-  sendSignupMail = (emailAddr) => {
+  sendSignupMail : (emailAddr) => {
 
     // TODO: kindly finish up the code
     const mailData = {
@@ -29,12 +30,25 @@ module.exports = {
   },
 
   /* code for Sign In mail here */
-  sendSigninMail = (emailAddr) => {
+  sendSigninMail : (emailAddr) => {
+    let time = dateFormat(new Date(), "yyyy-mm-dd h:MM:ss");
+    const msg = {
+      to: emailAddr,
+      from: process.env.SENDER_EMAIL,
+      subject: 'Avengers APP',
+      text: 'Hello, \nYou just signed into your account at ' + time + '\n Best Regards! ',
+     
+    };
+    return new Promise((resolve, reject) => {
+      sendGrid.send(msg).then(resolve).catch(reject);
+    
+    })
+    
 
   },
 
   /* code for Forgot Password mail here */
-  sendForgotpasswordMail = (emailAddr) => {
+  sendForgotpasswordMail : (emailAddr) => {
 
   }
 
