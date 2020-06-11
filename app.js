4e2c33be575e9ea22e0c02d1014cd59bfc505122
Dev Body <dasther@outlook.com>
2020-06-11 07:37:05 -0400

    Heroku Changes

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ const users = require('./routers/users.route')
 let db = process.env.MONGODB_URI
 
 //switch between docker image and atlas
-if (process.env.DOCKER_DB) {
-  db = process.env.DOCKER_DB;
-}
+//Please uncomment this to use to work for docker
+// if (process.env.DOCKER_DB) {
+//   db = process.env.DOCKER_DB;
+// }
 
 //db connection
 mongoose
