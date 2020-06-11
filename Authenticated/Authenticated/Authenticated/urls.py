68ae542baac3245b222566ae8870d9e4c6388965
Louieee <louis.paul9095@gmail.com>
2020-06-11 13:27:39 +0100

    @Louis amended the conflicts

--- Authenticated/Authenticated/Authenticated/urls.py
+++ Authenticated/Authenticated/Authenticated/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('admin/', admin.site.urls),
     path('', include('authentify.urls')),
-    path('accounts/', include('django.contrib.auth.urls')), #this allows the user login using django built in function
+    path('accounts/', include('django.contrib.auth.urls')), # this allows the user login using django built in function
     path('api/v1/auth/', include('dj_rest_auth.urls')),
     path('api/v1/auth/register', include('dj_rest_auth.registration.urls')),
     #path(r'^api-token-auth/', obtain_jwt_token),
